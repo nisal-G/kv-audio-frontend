a7d0012200268eed630f49a649cdff0f20c5a41e
nisalG <nimgunathilaka021@gmail.com>
2025-12-12 17:46:45 +0530

    Update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AiFillAccountBook } from "react-icons/ai";
 
 function App() {
   return (
     <>
-      <div className='bg-blue-500 w-[500px] h-[500px]'>
-        <div className="bg-yellow-100 w-[100px] h-[100px] ">
+      <div className= 'w-full h-screen flex'>
 
+        <div className="bg-green-100 w-[300px] h-full ">
         </div>
+
+        <div className="bg-blue-100 flex-1 h-full">
+          <AiFillAccountBook />
+        </div>
+
       </div>
     </>
   ) 

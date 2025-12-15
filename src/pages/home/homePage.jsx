c24c4f2f87cbe357f7fd9d46d97b1bca3f3fa417
nisalG <nimgunathilaka021@gmail.com>
2025-12-15 18:48:20 +0530

    Add error handling and update routing in Header component

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -4,6 +4,7 @@ import Contact from "./contactUs";
 import Home from "./home";
 import Gallery from "./gallery";
 import Items from "./items";
+import ErrorNotFound from "./error";
 
 export default function HomePage() {
     return (    
@@ -15,6 +16,7 @@ export default function HomePage() {
                     <Route path="/home" element={<Home />} />
                     <Route path="/gallery" element={<Gallery />} />
                     <Route path="/items" element={<Items />} />
+                    <Route path="/*" element={< ErrorNotFound />} />
                 </Routes>
                 
             </div>

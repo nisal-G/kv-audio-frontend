9388d8b5bd63d79b6a8137e5b86b05dca6fd942f
nisalG <nimgunathilaka021@gmail.com>
2025-12-16 11:43:10 +0530

    Add Login component and set up routing to the Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
+import Login from "./login/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/login/login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div 
+      className="w-full h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/Login_bg_Image.jpg')" }}
+    >
+      <h1>ssss</h1>
+    </div>
+  )
+}
+

a9ab66791eca115ec663a999805de2e14946b19d
nisalG <nimgunathilaka021@gmail.com>
2026-01-02 10:19:39 +0530

    Add Login and Register pages with routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
-import Login from "./login/login";
+import Login from "./pages/login/login";
 import { Toaster } from "react-hot-toast";
 import AdminPage from "./pages/admin/adminPage";
+import RegisterPage from "./pages/register/register";
 
 function App() {
   return (
     <BrowserRouter>
     <Toaster position="top-right" reverseOrder={false} />
-      <Routes>
+      <Routes path="/*">
         <Route path="/*" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/admin/*" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Register Page</h1>
+        </div>
+    );
+}   

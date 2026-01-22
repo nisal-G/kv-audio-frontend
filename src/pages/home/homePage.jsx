fb4a96af785a088baefacbc9999981dc44a0d3a0
nisalG <nimgunathilaka021@gmail.com>
2026-01-22 10:14:55 +0530

    Add Footer component and integrate it into HomePage layout

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../../components/header";
+import Footer from "../../components/Footer/Footer";
 import Contact from "./contactUs";
 import Home from "./home";
 import Gallery from "./gallery";
@@ -45,21 +46,22 @@ export default function HomePage() {
         );
     }
 
-    return (    
-        <div className="w-full h-screen flex flex-col items-center">
+
+    return (
+        <div className="w-full min-h-screen flex flex-col">
             <Header />
-            <div className="w-full h-[calc(100vh-4px) ">
+            <div className="w-full flex-1 overflow-y-auto">
                 <Routes path="/*">
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/gallery" element={<Gallery />} />
                     <Route path="/items" element={<Items />} />
-                    <Route path= "/product/:key" element={<ProductOverview />} />
-                    <Route path= "/booking" element={<BookingPage />} />
+                    <Route path="/product/:key" element={<ProductOverview />} />
+                    <Route path="/booking" element={<BookingPage />} />
                     <Route path="/*" element={< ErrorNotFound />} />
                 </Routes>
-                
             </div>
+            <Footer />
         </div>
     )
 }
